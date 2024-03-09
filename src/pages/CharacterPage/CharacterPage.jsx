538f05c6fcb29aa27a94438d4c6220e6193a0d55
Zeraku-silent <shaman-96@inbox.ru>
2024-03-10 01:21:34 +0700

    Try react rout

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -1,4 +1,12 @@
-import { Box, Card, Flex, Image, Link, Text } from "@chakra-ui/react";
+import {
+  GridItem,
+  Grid,
+  Box,
+  Image,
+  Heading,
+  Text,
+  Divider,
+} from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { characters } from "../../store/charactersReducer";
@@ -7,53 +15,129 @@ export const CharacterPage = () => {
   const { name } = useParams();
 
   const heroes = useSelector(characters);
+
   const character = heroes.find((hero) => hero.name === name);
 
+  console.log(character);
+
   return (
-    <Flex
-      flexDirection={"column"}
-      justify={"start"}
-      mt={150}
-      mx={"auto"}
-      width={"100%"}
-      h={"100vh"}
-      pt={5}
-      wrap={"wrap"}
-      gap={2}
-      alignItems={"center"}
-    >
-      <Card>
-        <Box alignSelf={"center"}>
-          <Image src={character.image} />
-        </Box>
-        <Box>
-          <Text>{character.name}</Text>
-        </Box>
-        <Box>
-          <Text>Раса: {character.species}</Text>
-        </Box>
-        <Box>
-          <Text>Статус: {character.status}</Text>
-        </Box>
-        <Box>
-          <Text>Пол: {character.gender}</Text>
-        </Box>
-        <Box>
-          <Text>Происхождение: {character.origin.name}</Text>
-          <Link>{character.origin.url}</Link>
-        </Box>
-        <Box>
-          <Text>
-            Текущее расположение:
-            <Link pointerEvents={"none"} href={character.location.url}>
-              {character.location.name}
-            </Link>
+    <Box mr={"auto"} ml={"auto"} p={5} w={"60%"}>
+      <Grid
+        borderRadius={40}
+        templateAreas={`'avatar info  info'
+        'episode episode  episode'`}
+        justifyContent={"center"}
+        bgGradient={[
+          "linear-gradient(290deg, gray.500, gray.500, #000)",
+          "radial-gradient(ellipse farthest-corner, gray.500 40%, #000 90%)",
+        ]}
+        w={"auto"}
+        h="100vh"
+        templateRows="auto 1fr"
+        templateColumns="1fr 1fr 1fr"
+        gap={5}
+        m={0}
+        mt={160}
+        p={7}
+      >
+        <GridItem
+          borderRadius={30}
+          area={"avatar"}
+          bg={"gray.700"}
+          shadow="6px -6px 15px black"
+        >
+          <Image
+            border={"solid 7px  "}
+            borderRadius={30}
+            w={"100%"}
+            h={"auto"}
+            src={character.image}
+          />
+        </GridItem>
+        <GridItem
+          pt={5}
+          borderRadius={30}
+          area={"info"}
+          bg={"gray.700"}
+          shadow="6px -6px 15px black"
+        >
+          <Heading size={"2xl"}>{character.name}</Heading>
+          <Divider h={6} />
+          <Text
+            bg={"gray.600"}
+            p={1}
+            pl={5}
+            borderRadius={25}
+            mr={5}
+            ml={5}
+            mt={4}
+            fontSize={"xl"}
+            textAlign={"left"}
+          >
+            Gender: {character.gender}
           </Text>
-        </Box>
-        <Box>
-          <Text>Был создан: {character.created}</Text>
-        </Box>
-      </Card>
-    </Flex>
+          <Text
+            bg={"gray.600"}
+            p={1}
+            pl={5}
+            borderRadius={25}
+            mr={5}
+            ml={5}
+            mt={4}
+            fontSize={"xl"}
+            textAlign={"left"}
+          >
+            Species: {character.species}
+          </Text>
+          <Text
+            bg={"gray.600"}
+            p={1}
+            pl={5}
+            borderRadius={25}
+            mr={5}
+            ml={5}
+            mt={4}
+            fontSize={"xl"}
+            textAlign={"left"}
+          >
+            Status: {character.status}
+          </Text>
+          <Text
+            bg={"gray.600"}
+            p={1}
+            pl={5}
+            borderRadius={25}
+            mr={5}
+            ml={5}
+            mt={4}
+            fontSize={"xl"}
+            textAlign={"left"}
+          >
+            Type: {character.type}
+          </Text>
+          <Text
+            bg={"gray.600"}
+            p={1}
+            pl={5}
+            borderRadius={25}
+            mr={5}
+            ml={5}
+            mt={4}
+            fontSize={"xl"}
+            textAlign={"left"}
+          >
+            Origin: {character.origin.name}
+          </Text>
+        </GridItem>
+        <GridItem
+          borderRadius={30}
+          area={"episode"}
+          bg={"gray.700"}
+          shadow="6px -6px 15px black"
+        >
+          sdf
+        </GridItem>
+      </Grid>
+    </Box>
   );
 };
